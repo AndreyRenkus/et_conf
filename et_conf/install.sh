408760baca03fc6a706c24b013f90aeecd71e189
Andrey Renkus <andrew@Zotek.(none)>
2015-01-26 14:44:12 +0200

    2

--- et_conf/install.sh
+++ et_conf/install.sh
@@ -6,9 +6,9 @@ INIT_DIR=/etc/init
 
 sudo -u $USER cp .profile $HOME_DIR
 sudo -u $USER mkdir $HOME_DIR/.config
-sudo -u $USER cp openbox/ $HOME_DIR/.config
-sudo -u $USER mkdir $HOME/Pictures
-sudo -u $USER cp eshtapay.jpg $HOME/Pictures
+sudo -u $USER cp -r openbox/ $HOME_DIR/.config
+sudo -u $USER mkdir $HOME_DIR/Pictures
+sudo -u $USER cp eshtapay.jpg $HOME_DIR/Pictures
 sudo -u $USER crontab crontabs/eshtapay
 
 cp etc/* $ETC_DIR
@@ -17,4 +17,5 @@ cp -r eshtapay/ /usr/local
 cp -r eshtapay-terminals/ /usr/local
 
 mkdir /usr/lib/jvm
-cp jre1.8.0_25/ /usr/lib/jvm
+cp -r jre1.8.0/ /usr/lib/jvm
+update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jre1.8.0/bin/java" 1
